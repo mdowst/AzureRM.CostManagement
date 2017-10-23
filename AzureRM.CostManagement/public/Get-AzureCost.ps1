7245c55587c98a1591d6fa6d7f37a80f23283fe6
Kilian Arjona <karjona@flexxible.com>
2017-10-23 17:48:56 +0200

    Create module and scaffold first function

---
+++ AzureRM.CostManagement/public/Get-AzureCost.ps1
@@ -0,0 +1,46 @@
+﻿Function Get-AzureCost {
+
+    <#
+    .SYNOPSIS
+    
+
+    .DESCRIPTION
+
+
+    .NOTES
+    Author:        Kilian Arjona
+    Version:       v0.1.0
+    Date released: 23/10/2017
+
+    .LINK
+    https://github.com/karjona/AzureRM.CostManagement
+
+    .PARAMETER 
+    
+
+    .EXAMPLE
+
+
+
+    .COMPONENT
+    AzureRM.CostManagement
+    #>
+
+    [CmdletBinding(DefaultParameterSetName='')]
+    
+        Param(
+        )
+
+
+    Begin {
+
+    }
+
+    Process {
+
+    }
+
+    End {
+
+    }
+}
---
+++ tests/Get-AzureCost.Tests.ps1
@@ -0,0 +1,8 @@
+﻿Get-Module -Name AzureRM.CostManagement -All | Remove-Module -Force -ErrorAction Ignore
+Import-Module -Name "$PSScriptRoot\..\AzureRM.CostManagement\AzureRM.CostManagement.psd1" -Force -ErrorAction Stop
+
+Describe "Get-AzureCost" {
+    It "does something useful" {
+        $true | Should Be $false
+    }
+}
